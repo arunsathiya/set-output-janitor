306322c5a29ebd64e244963dc69c8b773eb50a42
Arun <arun@arun.blog>
2024-01-13 02:27:34 -0800

    I think I have a complete setup now: check for PRs, if no PR, find and replace and commit, and one last check again

--- main.go
+++ main.go
@@ -33,47 +33,75 @@ func main() {
 				return
 			}
 
-			// Check for ::set-output in cloned directory
-			fmt.Println("Checking for ::set-output in", repoDir)
-			grepCmd := "grep -rnw '" + repoDir + "' -e '::set-output'"
-			grep := exec.Command("bash", "-c", grepCmd)
-			output, err := grep.Output()
+			// Check for existing PRs
+			fmt.Println("Checking for existing PRs in", repoDir)
+			prListCmd := "gh pr list --author \"@me\""
+			prList := exec.Command("bash", "-c", prListCmd)
+			prList.Dir = repoDir
+			prListOutput, err := prList.Output()
 			if err != nil {
-				fmt.Println("::set-output not found or error in grep:", err)
+				fmt.Println("Error in prList check:", err)
 				return
 			}
-			fmt.Printf("::set-output found in %s:\n%s\n", repoDir, output)
+			if len(prListOutput) == 0 {
+				// Check for ::set-output in cloned directory
+				fmt.Println("Checking for ::set-output in", repoDir)
+				grepCmd := "grep -rnw '" + repoDir + "' -e '::set-output'"
+				grep := exec.Command("bash", "-c", grepCmd)
+				if err := grep.Run(); err != nil {
+					fmt.Println("::set-output not found or error in grep:", err)
+					return
+				}
 
-			// Replace ::set-output command
-			fmt.Println("Replacing ::set-output in", repoDir)
-			findCmd := "find . -type f -name '*.yml' -exec sed -i '' 's/echo \"::set-output name=\\(.*\\)::\\(.*\\)\"/echo \"\\1=\\2\" >> $GITHUB_OUTPUT/g' {} +"
-			find := exec.Command("bash", "-c", findCmd)
-			find.Dir = repoDir
-			output, err = find.Output()
-			if err != nil {
-				fmt.Println("Error replacing ::set-output:", err)
-				return
-			}
+				// Replace ::set-output command
+				fmt.Println("Replacing ::set-output in", repoDir)
+				findReplaceCmd := "find . -type f -name '*.yml' -exec sed -i '' 's/echo \"::set-output name=\\(.*\\)::\\(.*\\)\"/echo \"\\1=\\2\" >> $GITHUB_OUTPUT/g' {} +"
+				findReplace := exec.Command("bash", "-c", findReplaceCmd)
+				findReplace.Dir = repoDir
+				if err := findReplace.Run(); err != nil {
+					fmt.Println("Error replacing ::set-output:", err)
+					return
+				}
 
-			// One more replace run
-			fmt.Println("Second replace run for ::set-output in", repoDir)
-			findCmd = "find . -type f -name '*.yml' -exec sed -i '' 's/echo ::set-output name=\\([^:]*\\)::\\(.*\\)/echo \"\\1=\\2\" >> \\$GITHUB_OUTPUT/g' {} +"
-			find = exec.Command("bash", "-c", findCmd)
-			find.Dir = repoDir
-			output, err = find.Output()
-			if err != nil {
-				fmt.Println("Error replacing ::set-output:", err)
-				return
-			}
+				// One more replace run
+				fmt.Println("Second replace run for ::set-output in", repoDir)
+				secondFindReplaceCmd := "find . -type f -name '*.yml' -exec sed -i '' 's/echo ::set-output name=\\([^:]*\\)::\\(.*\\)/echo \"\\1=\\2\" >> \\$GITHUB_OUTPUT/g' {} +"
+				secondFindReplace := exec.Command("bash", "-c", secondFindReplaceCmd)
+				secondFindReplace.Dir = repoDir
+				if err := secondFindReplace.Run(); err != nil {
+					fmt.Println("Error replacing ::set-output:", err)
+					return
+				}
 
-			// Commit changes
-			fmt.Println("Committing changes for", repoDir)
-			commitCmd := "git add . && git commit -m \"ci: Use GITHUB_OUTPUT envvar instead of set-output command\""
-			commit := exec.Command("bash", "-c", commitCmd)
-			commit.Dir = repoDir
-			if commit.Run(); err != nil {
-				fmt.Println("Error committing changes:", err)
-				return
+				// Commit changes
+				fmt.Println("Committing changes for", repoDir)
+				commitCmd := "git add . && git commit -m \"ci: Use GITHUB_OUTPUT envvar instead of set-output command\""
+				commit := exec.Command("bash", "-c", commitCmd)
+				commit.Dir = repoDir
+				if commit.Run(); err != nil {
+					fmt.Println("Error committing changes:", err)
+					return
+				}
+
+				// Check for ::set-output once more
+				fmt.Println("Check for ::set-output once more in", repoDir)
+				grepOnceMoreCmd := "grep -rnw '" + repoDir + "' -e '::set-output'"
+				grepOnceMore := exec.Command("bash", "-c", grepOnceMoreCmd)
+				grepOnceMore.Dir = repoDir
+				grepOnceMoreOutput, _ := grepOnceMore.Output()
+				if grepOnceMoreOutput != nil {
+					fmt.Printf("::set-output found in %s:\n%s\n", repoDir, grepOnceMoreOutput)
+				}
+
+				// Check for ::save-state in cloned directory
+				fmt.Println("Checking for ::save-state in", repoDir)
+				grepSaveStateCmd := "grep -rnw '" + repoDir + "' -e '::save-state'"
+				grepSaveState := exec.Command("bash", "-c", grepSaveStateCmd)
+				grepSaveState.Dir = repoDir
+				grepSaveStateOutput, _ := grepSaveState.Output()
+				if grepSaveStateOutput != nil {
+					fmt.Printf("::save-state found in %s:\n%s\n", repoDir, grepSaveStateOutput)
+				}
 			}
 		}(scanner.Text())
 	}
